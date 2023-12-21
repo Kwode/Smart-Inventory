fb0b511e648e5c0fdf1c5aab2e81040f51dbbada
Kwode <154435066+Kwode@users.noreply.github.com>
2023-12-21 18:49:47 +0100

    Update prototype.java

--- Neurolab Vault/neurolab_vault/src/prototype.java
+++ Neurolab Vault/neurolab_vault/src/prototype.java
@@ -1,1 +1,11 @@
+import java.util.HashMap;
+
+public class prototype {
+    public static void main(String[] args) {
+        HashMap<String, Integer> prototype1 = new HashMap<>();
+        prototype1.put("Paracetamol", 8);
+
+        System.out.println(prototype1);
+    }
+}
 
