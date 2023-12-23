51502e0fe22535c6cd28e097ac8b685f1c8af605
Kwode <154435066+Kwode@users.noreply.github.com>
2023-12-23 18:57:59 +0100

    Update prototype.java

--- Neurolab Vault/neurolab_vault/src/prototype.java
+++ Neurolab Vault/neurolab_vault/src/prototype.java
@@ -1,11 +1,34 @@
-import java.util.HashMap;
+import java.util.ArrayList;
+import java.util.Scanner;
 
-public class prototype {
+public class prototype{
     public static void main(String[] args) {
-        HashMap<String, Integer> prototype1 = new HashMap<>();
-        prototype1.put("Paracetamol", 8);
+        //initializing the scaner object
+        Scanner input = new Scanner(System.in);
 
-        System.out.println(prototype1);
+        //taking input from the user
+        System.out.println("What section do you want to work with today?");
+        System.out.println("1. Equipments");
+        System.out.println("2. Medicine");
+        System.out.println("\n");
+        System.out.println(".........");
+        int sectionNumber = input.nextInt();
+
+
+
+        switch (sectionNumber)
+        {
+            case 1:
+                System.out.println("You are in the equipment section");
+                System.out.println("What would you like to work with?");
+                break;
+
+            case 2:
+                System.out.println("You are in the medicine section");
+                System.out.println("What would you like to work with?");
+                break;
+        }
+
+
     }
 }
-
