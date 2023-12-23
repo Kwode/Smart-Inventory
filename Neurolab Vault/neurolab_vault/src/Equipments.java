6d1ff751ac8200a0c21d2b4009b1c6c4aab4b346
Kwode <154435066+Kwode@users.noreply.github.com>
2023-12-23 19:30:05 +0100

    Create Equipments.java

---
+++ Neurolab Vault/neurolab_vault/src/Equipments.java
@@ -0,0 +1,39 @@
+public class Equipments {
+    private int equipmentSelection;
+    void getInfo()
+    {
+        System.out.println("You are in the equipment section");
+        System.out.println("What would you like to work with?");
+        System.out.println("1. Consumables and accessories");
+        System.out.println("2. Spare Parts");
+        System.out.println("3. Medical Equipments");
+        System.out.println("4. Testing Equipments");
+
+        switch (equipmentSelection)
+        {
+            case 1:
+                System.out.println("Would you like to...");
+                System.out.println("1. Update Inventory");
+                System.out.println("2. Look into inventory");
+                break;
+            case 2:
+                System.out.println("Would you like to...");
+                System.out.println("1. Update Inventory");
+                System.out.println("2. Look into inventory");
+                break;
+            case 3:
+                System.out.println("Would you like to...");
+                System.out.println("1. Update Inventory");
+                System.out.println("2. Look into inventory");
+                break;
+            case 4:
+                System.out.println("Would you like to...");
+                System.out.println("1. Update Inventory");
+                System.out.println("2. Look into inventory");
+                break;
+            default:
+                System.out.println("Invalid Input");
+        }
+    }
+}
+
