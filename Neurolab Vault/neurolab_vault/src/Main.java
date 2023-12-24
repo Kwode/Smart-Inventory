d4585ce1fda29c9a1e4c4ea24b2cc646ee03d9a2
Kwode Great <154435066+Kwode@users.noreply.github.com>
2023-12-24 19:19:26 +0100

    Update Main.java

--- Neurolab Vault/neurolab_vault/src/Main.java
+++ Neurolab Vault/neurolab_vault/src/Main.java
@@ -2,36 +2,42 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main{
+    static Scanner input = new Scanner(System.in);
+
     //initializing the equipments class
     public static Equipments inventoryEquipment = new Equipments();
 
     //initializing the medicine class
     public static Medicine inventoryMedicine = new Medicine();
 
-    public static void main(String[] args) {
-        //initializing the scanner object
-        Scanner input = new Scanner(System.in);
-
-        //taking input from the user
+    public static void welcomeUser()
+    {
+        System.out.println("NEUROLAB VAULT");
         System.out.println("What section do you want to work with today?");
         System.out.println("1. Equipments");
         System.out.println("2. Medicine");
-        System.out.println("\n");
-        System.out.println(".........");
+        System.out.print("\n");
+
         int sectionNumber = input.nextInt();
 
         //using switch-case to choose sections
         switch (sectionNumber)
         {
             case 1:
-                inventoryEquipment.getInfo();
+                inventoryEquipment.displayOptions();
                 break;
             case 2:
                 System.out.println("You are in the medicine section");
                 System.out.println("What would you like to work with?");
                 break;
+            default:
+                System.out.println("Invalid Input");
+                break;
         }
 
+    }
 
+    public static void main(String[] args) {
+        welcomeUser();
     }
 }
